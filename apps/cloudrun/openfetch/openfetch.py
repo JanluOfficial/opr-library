6e0a86f59c5c69460c8a3dc185c84a742d2774f8
JanluOfficial <95766563+JanluOfficial@users.noreply.github.com>
2023-01-31 20:54:53 +0100

    Added OpenFetch

---
+++ apps/cloudrun/openfetch/openfetch.py
@@ -0,0 +1,18 @@
+import platform
+from rich import print
+import os
+
+print(f'[deep_pink3]OS:[/deep_pink3] {platform.uname().system}')
+print(f'[deep_pink3]PC Name:[/deep_pink3] {platform.uname().node}')
+print()
+print(f'[deep_pink3]Platform:[/deep_pink3] {platform.platform()}')
+print(f'[deep_pink3]Architecture:[/deep_pink3] {platform.machine()}')
+print(f'[deep_pink3]CPU:[/deep_pink3] {platform.processor()}')
+print(f'[deep_pink3]CPU Cores:[/deep_pink3] {os.cpu_count()}')
+print(f'[deep_pink3]Architecture:[/deep_pink3] {platform.machine()}')
+print()
+if os.name == "nt":
+    print(f'[deep_pink3]Windows Version:[/deep_pink3] Windows {platform.win32_ver()[0]}')
+    print(f'[deep_pink3]Build:[/deep_pink3] {platform.win32_ver()[1]}')
+    print(f'[deep_pink3]Windows Edition:[/deep_pink3] {platform.win32_edition()}')
+    print(f'[deep_pink3]Service Pack:[/deep_pink3] {platform.win32_ver()[2]}')
